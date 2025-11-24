9c4acb881651d0960f7edfa4a7766517549c7d3a
James0812 <james.roussel008@gmail.com>
2025-11-24 15:04:06 +0100

    add test

--- main.c
+++ main.c
@@ -20,6 +20,8 @@ int main(void)
     addr = (void *)0x7ffe637541f0;
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
+    _printf("%");
+     printf("\n======================\n");
     _printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
     _printf("Unsigned:[%u]\n", ui);
