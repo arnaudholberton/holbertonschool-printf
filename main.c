0f8a17d16a0a0c1e80f073da3f037c7e9fee11c5
James0812 <james.roussel008@gmail.com>
2025-11-24 12:41:59 +0100

    add the new code

--- main.c
+++ main.c
@@ -6,17 +6,9 @@
  */
 int main(void)
 {
-    /* Test _printf with NULL format (should not print anything) */
+    /* Optional: you can call _printf with NULL or empty string */
     _printf(NULL);
-
-    /* Test _printf with empty string (should not print anything) */
     _printf("");
-
-    /* Test with only literal characters */
-    _printf("This is a safe test.\n");
-
-    /* Test with characters and strings */
-    _printf("Character: %c, String: %s, Percent: %%\n", 'A', "Hello");
 
     /* End of main */
     return (0);
