c38790bc3d4d484548483f9f76016f13ad986d51
arnaudholberton <12052@holbertonstudents.com>
2025-11-27 12:31:09 +0100

    merge branch

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _printf - prints output according to a format
  * @format: format string
@@ -7,95 +6,59 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0, i = 0;
-    char c, *s;
-
-    if (!format)
-        return (-1);
-
-    va_start(args, format);
-
-    while (format[i] != '\0')
-    {
-        if (format[i] == '%')
-        {
-            i++;
-
-            if (format[i] == '\0')
-            {
-                va_end(args);
-                return (-1);
-            }
-
-            if (format[i] == 'c')
-            {
-                c = va_arg(args, int);
-                count += write(1, &c, 1);
-            }
-            else if (format[i] == 's')
-            {
-                s = va_arg(args, char *);
-                if (!s)
-                    s = "(null)";
-                while (*s)
-                {
-                    count += write(1, s, 1);
-                    s++;
-                }
-            }
-            else if (format[i] == 'd' || format[i] == 'i')
-            {
-                int n = va_arg(args, int);
-                unsigned int num;
-                char buffer[12];
-                int j = 0;
-
-                if (n < 0)
-                {
-                    count += write(1, "-", 1);
-                    num = -n;
-                }
-                else
-                {
-                    num = n;
-                }
-
-                if (num == 0)
-                {
-                    count += write(1, "0", 1);
-                }
-                else
-                {
-                    while (num > 0)
-                    {
-                        buffer[j++] = (num % 10) + '0';
-                        num /= 10;
-                    }
-
-                    while (j--)
-                    {
-                        count += write(1, &buffer[j], 1);
-                    }
-                }
-            }
-            else if (format[i] == '%')
-            {
-                count += write(1, "%", 1);
-            }
-            else
-            {
-                count += write(1, "%", 1);
-                count += write(1, &format[i], 1);
-            }
-        }
-        else
-        {
-            count += write(1, &format[i], 1);
-        }
-        i++;
-    }
-
-    va_end(args);
-    return (count);
+va_list args;
+int count = 0, i = 0, num;
+char c, *s;
+if (!format)
+return (-1);
+va_start(args, format);
+while (format[i] != '\0')
+{
+	if (format[i] == '%')
+	{
+		i++;
+		if (format[i] == '\0')
+		{
+			va_end(args);
+			return (-1);
+		}
+		if (format[i] == 'c')
+		{
+			c = va_arg(args, int);
+			count += write(1, &c, 1);
+		}
+		else if (format[i] == 's')
+		{
+			s = va_arg(args, char *);
+		if (!s)
+			s = "(null)";
+		while (*s)
+		{
+			count += write(1, s, 1);
+			s++;
+		}
+		}
+		else if (format[i] == 'd' || format[i] == 'i')
+		{
+			num = va_arg(args, int);
+			count += printd(num);
+		}
+		else if (format[i] == '%')
+		{
+			count += write(1, "%", 1);
+		}
+		else
+		{
+			count += write(1, "%", 1);
+			count += write(1, &format[i], 1);
+		}
+	}
+	else
+	{
+		count += write(1, &format[i], 1);
+	}
+	i++;
+}
+va_end(args);
+return (count);
 }
---
+++ printd.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * printd - prints a decimal (integer) number
+ * @num: the integer to print
+ * Return: number of characters printed
+ */
+int printd(int num)
+{
+int count = 0;
+int buffer[12];
+int j = 0;
+if (num < 0)
+{
+	count += write(1, "-", 1);
+	num = -num;
+}
+else
+{
+	num = num;
+}
+if (num == 0)
+{
+	count += write(1, "0", 1);
+}
+else
+{
+	while (num > 0)
+	{
+		buffer[j++] = (num % 10) + '0';
+		num /= 10;
+	}
+	while (j--)
+	{
+		count += write(1, &buffer[j], 1);
+	}
+}
+return (count);
+}
